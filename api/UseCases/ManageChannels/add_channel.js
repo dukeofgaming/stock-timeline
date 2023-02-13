bc4d5453ea416a14fd65306092acefde69567abb
David Vega <dukeofgaming@gmail.com>
2023-02-13 08:36:49 -0600

    Testing script

--- api/UseCases/ManageChannels/add_channel.js
+++ api/UseCases/ManageChannels/add_channel.js
@@ -4,36 +4,38 @@ const cli_args = require('minimist')(process.argv.slice(2));
 
 console.log(cli_args);
 
+let path = '@github/stock-timeline/api/UseCases/ManageChannels.yaml';
+// let path = '@github/gitrows/data/iris.json';
 
-const gitrows = new Gitrows();
+const gitrows = new Gitrows({
+    token       : process.env.GH_TOKEN,
+    username    : process.env.GH_USERNAME,
+});
+
 
 console.log(gitrows.options());
 
-// let path = '@github/gitrows/data/iris.json';
-let path = '.';
+try{
+    gitrows.put(
+        path, 
+        {
+            id          : cli_args.id,
+            name        : cli_args.name,
+            base_url    : cli_args.base_url
+        }
+    
+    ).catch(
+        (err) => {
+            console.log('Method error:');
+            console.log(err);
+        }
+    ).then(
+        (res) => {
+            console.log('Method success:');
+            console.log(res);
+        }
+    );
 
-console.log(gitrows.get(path).then(
-    data => {
-        console.log(data);
-    }
-));
-
-
-// new Gitrows({}).update(
-//     './ManageChannels.yaml', 
-//     {
-//         id          : cli_args.id,
-//         name        : cli_args.name,
-//         base_url    : cli_args.base_url
-//     },
-//     'id'
-
-// ).catch(
-//     (err) => {
-//         console.log(err);
-//     }
-// ).then(
-//     (res) => {
-//         console.log(res);
-//     }
-// );
+}catch(err){
+    console.log('Class error' + err);
+}
